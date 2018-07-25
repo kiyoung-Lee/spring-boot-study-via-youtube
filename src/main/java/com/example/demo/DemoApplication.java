99c0f0f53c6641e3a7afa63dccd92d13d2218bc9
KiyoungLee <eevee300@gmail.com>
2018-07-25 13:30:20 +0900

    7일차 업로드

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import javafx.application.Application;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
@@ -10,7 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@Import(ServiceCofig.class) - 이와같이 bean을 등록 가능하다.
 public class DemoApplication {
 
+
+	@Bean
+	public ExitCodeGenerator exitCodeGenerator(){
+		return () -> 42;
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		SpringApplication application = new SpringApplication(Application.class);
+//		application.setWebApplicationType(WebApplicationType.REACTIVE);
+//		application.setApplicationContextClass();
+
+//		System.exit(SpringApplication.exit(SpringApplication.run(DemoApplication.class, args)));
 	}
 }
