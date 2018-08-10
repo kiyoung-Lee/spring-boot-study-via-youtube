32bc24e1a32cb19d077368ccd954669e16195b8f
KiyoungLee <eevee300@gmail.com>
2018-08-10 14:31:54 +0900

    10일차 업로드

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.day06.MyListener;
+import com.example.demo.day09.SampleProperties;
 import javafx.application.Application;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -20,6 +22,16 @@ public class DemoApplication {
 	public ExitCodeGenerator exitCodeGenerator(){
 		return () -> 42;
 	}
+
+	// third party configuration 을 bean으로 주입 받을때
+	// 아래와 같이 등록하면 third party lib property를 가져올수 있다.
+//	@Bean
+//	@ConfigurationProperties("whiteship")
+//	public SampleProperties sampleProperties(){
+//		return new SampleProperties();
+//	}
+
+
 
 
 	public static void main(String[] args) {
